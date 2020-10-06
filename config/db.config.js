2a3a9c38d145ab62e889c130da86a9ccc58dac56
samundson101 <samundson@westmont.edu>
2020-10-06 14:42:47 -0700

    adding procfile

--- config/db.config.js
+++ config/db.config.js
@@ -6,9 +6,9 @@
 //};
 //dev database
 module.exports = {
-  database : "test",
-  user : "root",
-  host : "localhost",
-  pass : "vasejam1",
+  database : "heroku_a18441a3eb52d5c",
+  user : "bdd59ac6cdfb41",
+  host : "us-cdbr-east-02.cleardb.com",
+  pass : "10a3e7dd",
   port : 3006
 }
