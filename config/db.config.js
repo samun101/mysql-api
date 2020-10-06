daee63fd05a6d6662ebd53b276e93dde01763255
samundson101 <samundson@westmont.edu>
2020-10-06 16:33:22 -0700

    I don't know what is wrong

--- config/db.config.js
+++ config/db.config.js
@@ -5,6 +5,6 @@ module.exports = {
   database : "heroku_a18441a3eb52d5c",
   user : "bdd59ac6cdfb41",
   host : "us-cdbr-east-02.cleardb.com",
-  password : "10a3e7dd",
-  port : 3306
+  pass : "10a3e7dd",
+  //port : 3306
 }
