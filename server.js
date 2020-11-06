204c03d6ece61ba052cf30a69a322064960c14aa
samundson101 <samundson@westmont.edu>
2020-11-05 20:31:14 -0800

    trying to fix CORS request issues

--- server.js
+++ server.js
@@ -23,3 +23,10 @@ require("./apps/requirements.routing.js")(app);
 app.listen(app.get('port'), () => {
   console.log(`Server is running on port ${app.get('port')}`);
 });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
