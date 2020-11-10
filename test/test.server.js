b935feaea90741dc65a9a934b9110588a53d25be
samundson101 <samundson@westmont.edu>
2020-11-09 20:55:46 -0800

    getting testing going for each endpoint I have setup

--- test/test.server.js
+++ test/test.server.js
@@ -27,6 +27,7 @@ describe("testing schedule endpoints",()=>{
     it("testing schedule.selectbyID endpoint", done=>{
       chai.request(app).get("/schedule1").end(function(err,res){
         expect(res).to.have.status(200);
+        expect(res.body[0]).to.have.property("idschedules").to.equal(1);
         expect(res.body[0]).to.have.property("idschedules");
         expect(res.body[0]).to.have.property("schedulesName");
         expect(res.body).to.be.a('array');
@@ -43,8 +44,17 @@ describe("testing schedule endpoints",()=>{
         done();
       })
     })
+    it("testing requirements.selectbyMajor endpoint", done=>{
+      chai.request(app).get("/requirements1").end(function(err,res){
+        expect(res).to.have.status(200);
+        expect(res.body[0]).to.have.property("idRequirements").to.equal(1);
+        expect(res.body[0]).to.have.property("idRequirements");
+        expect(res.body).to.be.a('array');
+        done();
+      })
+    })
   })
-  describe("testing users endpoints",function(){
+describe("testing users endpoints",function(){
     it("testing users.getAll endpoint", done=>{
       chai.request(app).get("/user").end(function(err,res){
         expect(res).to.have.status(200);
@@ -53,4 +63,32 @@ describe("testing schedule endpoints",()=>{
         done();
       })
     })
+    it("testing users.selectbyIdUser endpoint", done=>{
+      chai.request(app).get("/user1").end(function(err,res){
+        expect(res).to.have.status(200);
+        expect(res.body[0]).to.have.property("idusers").to.equal(1);
+        expect(res.body[0]).to.have.property("idusers");
+        expect(res.body).to.be.a('array');
+        done();
+      })
+    })
   })
+  describe("testing years endpoints",function(){
+      it("testing year.getAll endpoint", done=>{
+        chai.request(app).get("/years").end(function(err,res){
+          expect(res).to.have.status(200);
+          expect(res.body[0]).to.have.property("idyears");
+          expect(res.body).to.be.a('array');
+          done();
+        })
+      })
+      it("testing year.selectbyID endpoint", done=>{
+        chai.request(app).get("/years1").end(function(err,res){
+          expect(res).to.have.status(200);
+          expect(res.body[0]).to.have.property("idyears").to.equal(1);
+          expect(res.body[0]).to.have.property("idyears");
+          expect(res.body).to.be.a('array');
+          done();
+        })
+      })
+    })
