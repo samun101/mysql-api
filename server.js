5401f6e40ab91d4a9fa078b7624618606db6850b
samundson101 <samundson@westmont.edu>
2020-12-10 21:15:49 -0800

    trying another thing with the CORS preflight=headers

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 //dealing with CORS stuff, maybe
 app.all('/',function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//    res.header("Access-Control-Allow-Credentials", true);
+//    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", '*');
     next();
 });
 
