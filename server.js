43e2912fbbd6b51f65dfca284033696273e1f48e
samundson101 <samundson@westmont.edu>
2020-12-11 10:27:53 -0800

    still working on CORS errors

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const bodyParser = require("body-parser");
 const app = express();
 
 //dealing with CORS stuff, maybe
-app.all('/',function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
-//    res.header("Access-Control-Allow-Credentials", true);
-//    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", '*');
     next();
 });
